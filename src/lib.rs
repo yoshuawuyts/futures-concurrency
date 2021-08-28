412e2ff239c20b45e343c31dede41f82c17ab3ee
Yoshua Wuyts <yoshuawuyts@gmail.com>
2021-08-28 16:44:31 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+//! Concurrency extensions for Future
+//!
+//! # Examples
+//!
+//! ```
+//! // tbi
+//! ```
+
+#![forbid(unsafe_code, rust_2018_idioms)]
+#![deny(missing_debug_implementations, nonstandard_style)]
+#![warn(missing_docs, future_incompatible, unreachable_pub)]
