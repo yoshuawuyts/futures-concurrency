9b671f72d502b04414aba79b43d67f15ad659ac6
Philipp Oppermann <dev@phil-opp.com>
2022-02-11 19:17:35 +0100

    Move the internal `utils::pin_project_array` function behind a feature gate
    The function appears to be unused currently.

--- src/lib.rs
+++ src/lib.rs
@@ -128,7 +128,7 @@
 #![deny(missing_debug_implementations, nonstandard_style)]
 #![warn(missing_docs, unreachable_pub)]
 #![allow(non_snake_case)]
-#![feature(array_methods)]
+#![cfg_attr(feature = "unstable", feature(array_methods))]
 
 mod join;
 mod merge;
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -12,5 +12,8 @@ mod rng;
 
 pub(crate) use fuse::Fuse;
 pub(crate) use maybe_done::MaybeDone;
-pub(crate) use pin::{get_pin_mut, get_pin_mut_from_vec, iter_pin_mut, pin_project_array};
+pub(crate) use pin::{get_pin_mut, get_pin_mut_from_vec, iter_pin_mut};
 pub(crate) use rng::random;
+
+#[cfg(feature = "unstable")]
+pub(crate) use pin::pin_project_array;
