3aa67f0020c541584f628123fb88b9eb4a95462d
Philipp Oppermann <dev@phil-opp.com>
2022-02-28 21:54:08 +0100

    Create recursive `poll_in_order` macro to fix tuple merge

--- src/merge/tuple.rs
+++ src/merge/tuple.rs
@@ -6,6 +6,22 @@ use futures_core::Stream;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
+macro_rules! poll_in_order {
+    ($cx:expr, $stream:expr) => { $stream.poll_next($cx) };
+    ($cx:expr, $stream:expr, $($next:tt),*) => {{
+        let mut pending = false;
+        match $stream.poll_next($cx) {
+            Poll::Ready(Some(item)) => return Poll::Ready(Some(item)),
+            Poll::Pending => { pending = true; }
+            Poll::Ready(None) => {},
+        }
+        match poll_in_order!($cx, $($next),*) {
+            Poll::Ready(None) if pending => Poll::Pending,
+            other => other,
+        }
+    }};
+}
+
 impl<T, S0, S1> MergeTrait for (S0, S1)
 where
     S0: IntoStream<Item = T>,
@@ -59,28 +75,10 @@ where
         let s0 = unsafe { Pin::new_unchecked(&mut this.streams.0) };
         let s1 = unsafe { Pin::new_unchecked(&mut this.streams.1) };
         match utils::random(2) {
-            0 => poll_next_in_order(s0, s1, cx),
-            1 => poll_next_in_order(s1, s0, cx),
+            0 => poll_in_order!(cx, s0, s1),
+            1 => poll_in_order!(cx, s1, s0),
             _ => unreachable!(),
         }
-    }
-}
-fn poll_next_in_order<S0, S1, T>(
-    s0: Pin<&mut S0>,
-    s1: Pin<&mut S1>,
-    cx: &mut Context<'_>,
-) -> Poll<Option<T>>
-where
-    S0: Stream<Item = T>,
-    S1: Stream<Item = T>,
-{
-    match s0.poll_next(cx) {
-        Poll::Ready(None) => s1.poll_next(cx),
-        Poll::Ready(item) => Poll::Ready(item),
-        Poll::Pending => match s1.poll_next(cx) {
-            Poll::Ready(None) | Poll::Pending => Poll::Pending,
-            Poll::Ready(item) => Poll::Ready(item),
-        },
     }
 }
 
@@ -117,6 +115,7 @@ pub struct Merge3<T, S0, S1, S2>
 where
     S0: Stream<Item = T>,
     S1: Stream<Item = T>,
+    S2: Stream<Item = T>,
 {
     streams: (S0, S1, S2),
 }
@@ -141,42 +140,18 @@ where
     type Item = T;
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        fn poll_next_in_order<S0, S1, S2, T>(
-            s0: Pin<&mut S0>,
-            s1: Pin<&mut S1>,
-            s2: Pin<&mut S2>,
-            cx: &mut Context<'_>,
-        ) -> Poll<Option<T>>
-        where
-            S0: Stream<Item = T>,
-            S1: Stream<Item = T>,
-            S2: Stream<Item = T>,
-        {
-            match s0.poll_next(cx) {
-                Poll::Ready(None) => s1.poll_next(cx),
-                Poll::Ready(item) => Poll::Ready(item),
-                Poll::Pending => match s1.poll_next(cx) {
-                    Poll::Ready(None) => s2.poll_next(cx),
-                    Poll::Ready(item) => Poll::Ready(item),
-                    Poll::Pending => match s2.poll_next(cx) {
-                        Poll::Ready(None) | Poll::Pending => Poll::Pending,
-                        Poll::Ready(item) => Poll::Ready(item),
-                    },
-                },
-            }
-        }
         let this = self.project();
         // SAFETY: we're manually projecting the tuple fields here.
         let s0 = unsafe { Pin::new_unchecked(&mut this.streams.0) };
         let s1 = unsafe { Pin::new_unchecked(&mut this.streams.1) };
         let s2 = unsafe { Pin::new_unchecked(&mut this.streams.2) };
         match utils::random(6) {
-            0 => poll_next_in_order(s0, s1, s2, cx),
-            1 => poll_next_in_order(s0, s2, s1, cx),
-            2 => poll_next_in_order(s1, s0, s2, cx),
-            3 => poll_next_in_order(s1, s2, s0, cx),
-            4 => poll_next_in_order(s2, s0, s1, cx),
-            5 => poll_next_in_order(s2, s1, s0, cx),
+            0 => poll_in_order!(cx, s0, s1, s2),
+            1 => poll_in_order!(cx, s0, s2, s1),
+            2 => poll_in_order!(cx, s1, s0, s2),
+            3 => poll_in_order!(cx, s1, s2, s0),
+            4 => poll_in_order!(cx, s2, s0, s1),
+            5 => poll_in_order!(cx, s2, s1, s0),
             _ => unreachable!(),
         }
     }
