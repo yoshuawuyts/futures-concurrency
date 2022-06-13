40a59c033353682cb05da371be6ffd6dd7339196
Yoshua Wuyts <yoshuawuyts@gmail.com>
2022-06-14 01:25:19 +0200

    split IntoStream into separate file

--- src/stream/mod.rs
+++ src/stream/mod.rs
@@ -3,6 +3,9 @@
 use crate::Merge;
 
 use futures_core::Stream;
+pub use into_stream::IntoStream;
+
+mod into_stream;
 
 /// Extend `Stream` with concurrency methods.
 pub trait StreamExt: Stream {
@@ -45,32 +48,3 @@ pub trait StreamExt: Stream {
 }
 
 impl<S> StreamExt for S where S: Stream {}
-
-/// Conversion into a `Stream`.
-///
-/// By implementing `IntoStream` for a type, you define how it will be
-/// converted to an iterator. This is common for types which describe a
-/// collection of some kind.
-///
-/// [`from_stream`]: #tymethod.from_stream
-/// [`Stream`]: trait.Stream.html
-pub trait IntoStream {
-    /// The type of the elements being iterated over.
-    type Item;
-
-    /// Which kind of stream are we turning this into?
-    type IntoStream: Stream<Item = Self::Item>;
-
-    /// Creates a stream from a value.
-    fn into_stream(self) -> Self::IntoStream;
-}
-
-impl<S: Stream> IntoStream for S {
-    type Item = S::Item;
-    type IntoStream = S;
-
-    #[inline]
-    fn into_stream(self) -> S {
-        self
-    }
-}
