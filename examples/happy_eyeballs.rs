84595b0dc3ec9e609c7f04c35cb4202d87859435
Yoshua Wuyts <2467194+yoshuawuyts@users.noreply.github.com>
2022-11-18 16:22:56 +0100

    Rework comments, remove final timeout

--- examples/happy_eyeballs.rs
+++ examples/happy_eyeballs.rs
@@ -5,51 +5,45 @@ use futures_time::prelude::*;
 
 use async_std::net::TcpStream;
 use futures::channel::oneshot;
+use futures_concurrency::vec::AggregateError;
 use futures_time::time::Duration;
-use std::error::Error;
+use std::{error, io};
 
 #[async_std::main]
-async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
+async fn main() -> Result<(), Box<dyn error::Error + Send + Sync + 'static>> {
+    // Connect to a socket
     let mut socket = open_tcp_socket("rust-lang.org", 80, 3).await?;
+
+    // Make an HTTP GET request.
     socket.write_all(b"GET / \r\n").await?;
     let mut res = String::new();
     socket.read_to_string(&mut res).await?;
     println!("{res}");
+
     Ok(())
 }
 
 /// Happy eyeballs algorithm!
-///
-/// 1. Create an iterator for the number of attempts
-/// 2. Create a `TcpStream` for each attempt
-/// 3. Delay the start of each tcp stream by `N` seconds
-/// 4. Collect into a vector of futures
-/// 5. Concurrently execute all futures using "race_ok" semantics -
-///    on failure, try the next future. Return on success or if all futures
-///    fail.
-/// 6. Set a timeout for our entire futures construct, so we stop all at the same time.
 async fn open_tcp_socket(
     addr: &str,
     port: u16,
     attempts: u64,
-) -> Result<TcpStream, Box<dyn Error + Send + Sync + 'static>> {
+) -> Result<TcpStream, AggregateError<io::Error>> {
     let (mut sender, mut receiver) = oneshot::channel();
     let mut futures = Vec::with_capacity(attempts as usize);
 
     for attempt in 0..attempts {
+        // Start a next attempt if the previous one finishes, or timeout expires.
         let tcp = TcpStream::connect((addr, port));
         let start_event = receiver.timeout(Duration::from_secs(attempt));
         futures.push(tcp.delay(start_event).map_err(|err| {
+            // If the socket fails, start the next attempt
             let _ = sender.send(());
             err
         }));
         (sender, receiver) = oneshot::channel();
     }
 
-    let socket = futures
-        .race_ok()
-        .timeout(Duration::from_secs(attempts + 1))
-        .await??;
-
-    Ok(socket)
+    // Start connecting. If an attempt succeeds, cancel all others attempts.
+    Ok(futures.race_ok().await?)
 }
