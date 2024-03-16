294942a4370695513fc06925a024c3a0e6b1d755
Yosh <github@yosh.is>
2024-03-16 17:54:06 +0100

    concurrent driving of streams

--- src/concurrent_stream/into_concurrent_iterator.rs
+++ src/concurrent_stream/into_concurrent_iterator.rs
@@ -1,6 +1,7 @@
+use crate::prelude::*;
 use futures_lite::{Stream, StreamExt};
-use std::future::Ready;
-use std::{future::ready, pin::pin};
+use std::future::{ready, Ready};
+use std::pin::pin;
 
 use super::{ConcurrentStream, Consumer};
 
@@ -24,11 +25,48 @@ where
         C: Consumer<Self::Item, Self::Future>,
     {
         let mut iter = pin!(self.iter);
-        while let Some(item) = iter.next().await {
-            consumer.send(ready(item)).await;
+
+        // Concurrently progress the consumer as well as the stream. Whenever
+        // there is an item from the stream available, we submit it to the
+        // consumer and we wait.
+        //
+        // NOTE(yosh): we're relying on the fact that `Stream::next` can be
+        // dropped and recreated freely. That's also true for
+        // `Consumer::progress`; though that is intentional. It should be
+        // possible to write a combinator which does not drop the `Stream::next`
+        // future repeatedly. However for now we're happy to rely on this
+        // property here.
+        loop {
+            // Drive the stream forward
+            let a = async {
+                let item = iter.next().await;
+                State::Item(item)
+            };
+
+            // Drive the consumer forward
+            let b = async {
+                consumer.progress().await;
+                State::Progress
+            };
+
+            // If an item is available, submit it to the consumer and wait for
+            // it to be ready.
+            match (a, b).race().await {
+                State::Progress => continue,
+                State::Item(Some(item)) => consumer.send(ready(item)).await,
+                State::Item(None) => break,
+            }
         }
+
+        // We will no longer receive items from the underlying stream, which
+        // means we're ready to wait for the consumer to finish up.
         consumer.finish().await
     }
+}
+
+enum State<T> {
+    Progress,
+    Item(T),
 }
 
 /// Convert into a concurrent stream
